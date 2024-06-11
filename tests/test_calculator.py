c7b0c5c27c7bd62a83034ff52011a2b9888ba76d
kburger <burger.github@gmail.com>
2024-06-11 12:27:37 +0200

    feat: add test cases for subtract, multiply, and divide

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,3 +8,15 @@ from simple_calculator.calculator import Calculator
 def test_add():
     calculator = Calculator()
     assert calculator.add(3, 7) == 10
+
+def test_subtract():
+    calculator = Calculator()
+    assert calculator.subtract(1, 7) == -6
+
+def test_multiply():
+    calculator = Calculator()
+    assert calculator.multiply(7, 7) == 49
+
+def test_divide():
+    calculator = Calculator()
+    assert calculator.divide(49, 7) == 7
